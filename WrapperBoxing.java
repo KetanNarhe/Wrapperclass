be9f2e305a085dcc0560a2f57f4161b65868459d
Ketan narhe <ketannarhe02@gmail.com>
2025-08-04 19:04:25 +0530

    Add files via upload

---
+++ WrapperBoxing.java
@@ -0,0 +1,22 @@
+package wrapper;
+
+public class WrapperBoxing {
+
+	public static void main(String[] args) {
+		
+		int i=100;
+		
+		Integer li=new Integer(i);
+		System.out.println(li);
+		
+		boolean b=true;
+		Boolean b1=new Boolean(b);
+		System.out.println(b1);
+		
+		Double d=10.20;
+		Double d1=new Double(d);
+		System.out.println(d1);
+
+	}
+
+}
