1b3d42e34a0d43e0c25e7115a5bcd16e51fa0c3c
Ketan narhe <ketannarhe02@gmail.com>
2025-08-04 23:01:00 +0530

    Add files via upload

---
+++ Wrapperclass1.java
@@ -0,0 +1,17 @@
+package wrapper;
+
+public class Wrapperclass1 {
+
+	public static void main(String[] args) {
+		
+		Integer i1=new Integer(100);
+		Integer i2=new Integer(100);
+        System.out.println(i1==i2);
+        
+        System.out.println(i1.equals(i2));
+        System.out.println(i1.hashCode(i2));
+       
+		
+	}
+
+}
